9a24d4ba80eb46a33978a0db50a61467bd5985ab
Gianlucca <106834389+sbritom@users.noreply.github.com>
2026-08-21 18:27:56 -0300

    replace bingo banner with complete black and gold artwork

--- src/assets/events/eventBanners.js
+++ src/assets/events/eventBanners.js
@@ -1,8 +1,4 @@
 import rankingBanner from './ranking-banner.webp'
-import bingoBannerPart1 from './bingoBannerPart1'
-import bingoBannerPart2 from './bingoBannerPart2'
-import bingoBannerPart3 from './bingoBannerPart3'
-
-const bingoBanner = `data:image/webp;base64,${bingoBannerPart1}${bingoBannerPart2}${bingoBannerPart3}`
+import bingoBanner from './bingo-banner.webp'
 
 export { rankingBanner, bingoBanner }
